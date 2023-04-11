1e34f146636876a1ed03cf08b5e993501e1a406c
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-04-12 06:42:22 +0900

    エディタにplaceholderを追加

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -84,6 +84,29 @@ export const Editor: React.FC<Props> = (props) => {
         <TextArea
           onChange={(event) => setText(event.target.value)}
           value={text}
+          placeholder="#　　今日はいい天気です
+
+Marked - Markdown Parser
+========================
+
+[Marked] lets you convert [Markdown] into HTML.  Markdown is a simple text format whose goal is to be very easy to read and write, even when not converted to HTML.  This demo page will let you type anything you like and see how it gets converted.  Live.  No more waiting around.
+
+How To Use The Demo
+-------------------
+
+1. Type in stuff on the left.
+2. See the live updates on the right.
+
+That's it.  Pretty simple.  There's also a drop-down option above to switch between various views:
+
+- **Preview:**  A live display of the generated HTML as it would render in a browser.
+- **HTML Source:**  The generated HTML before your browser makes it pretty.
+- **Lexer Data:**  What [marked] uses internally, in case you like gory stuff like this.
+- **Quick Reference:**  A brief run-down of how to format things using markdown.
+
+Why Markdown?
+-------------
+"
         />
         <Preview>
           <div dangerouslySetInnerHTML={{ __html: html }} />
